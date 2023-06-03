23f0eaa4508fba7433b71245beec01d53496930a
Kassio Wallace <kassiowl34@gmail.com>
2023-06-03 18:43:00 -0300

    router add

--- src/static_tsx/tsx/footer.tsx
+++ src/static_tsx/tsx/footer.tsx
@@ -5,6 +5,7 @@ import Medium from "../social_media_icons/medium.png"
 import Python from "../tecnology_stacks_icons/python.png"
 import Django from "../tecnology_stacks_icons/django.png"
 import Aws from "../tecnology_stacks_icons/aws.png"
+import { Link, BrowserRouter } from 'react-router-dom';
 
 function Footer() {
     return (
@@ -13,29 +14,45 @@ function Footer() {
                 <div className="social-media">
                     <h3>Social media</h3>
                     <div className="icon-container">
+                        <BrowserRouter>
                         <div className="icon">
-                            <img src={Linkedin} alt="linkedin" width={"70px"} height={"70px"}/>
+                            <Link to={"https://www.linkedin.com/in/kassiowallace/"} target="_blank">
+                                <img src={Linkedin} alt="linkedin" width={"70px"} height={"70px"}/>
+                            </Link>
                         </div>
                         <div className="icon">
-                            <img src={Medium} alt="medium" width={"70px"} height={"70px"}/>
+                            <Link to={"https://medium.com/@kassiowl34"} target="_blank"> 
+                                <img src={Medium} alt="medium" width={"70px"} height={"70px"}/>
+                            </Link>
                         </div>
                         <div className="icon">
-                            <img src={Github} alt="github" width={"70px"} height={"70px"}/>
+                            <Link to={"https://github.com/Kassiowl"} target="_blank">
+                                <img src={Github} alt="github" width={"70px"} height={"70px"}/>
+                            </Link>
                         </div>
+                        </BrowserRouter>
                     </div>
                 </div>
                 <div className="tecnology-stacks">
                     <h3>Tecnology Stacks</h3>
                     <div className="icon-container">
-                        <div className="icon">
-                            <img src={Python} alt="python" width={"70px"} height={"70px"}/>
-                        </div>
-                        <div className="icon">
-                            <img src={Django} alt="Django" width={"70px"} height={"70px"}/>
-                        </div>
-                        <div className="icon">
-                            <img src={Aws} alt="aws" width={"70px"} height={"70px"}/>
-                        </div>
+                        <BrowserRouter>
+                            <div className="icon">
+                                <Link to={"https://www.python.org/"} target="_blank" >
+                                    <img src={Python} alt="python" width={"70px"} height={"70px"}/>
+                                </Link>
+                            </div>
+                            <div className="icon">
+                                <Link to={"https://www.djangoproject.com/"} target="_blank" >
+                                    <img src={Django} alt="Django" width={"70px"} height={"70px"}/>
+                                </Link>
+                            </div>
+                            <div className="icon">
+                                <Link to={"https://aws.amazon.com/pt/"} target="_blank" >
+                                    <img src={Aws} alt="aws" width={"70px"} height={"70px"}/>
+                                </Link>
+                            </div>
+                        </BrowserRouter>
                     </div>
                 </div>
             </footer>
