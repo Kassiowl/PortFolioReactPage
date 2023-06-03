c2fefb546f6a28cd0f39e3a23392e239f401bb85
Kassio Wallace <kassiowl34@gmail.com>
2023-06-03 14:22:26 -0300

    tecnology stacks add

--- src/static_tsx/tsx/footer.tsx
+++ src/static_tsx/tsx/footer.tsx
@@ -2,6 +2,10 @@ import "../css/footer.css"
 import Linkedin from "../social_media_icons/linkedin.png"
 import Github from "../social_media_icons/github.png"
 import Medium from "../social_media_icons/medium.png"
+import Python from "../tecnology_stacks_icons/python.png"
+import Django from "../tecnology_stacks_icons/django.png"
+import Aws from "../tecnology_stacks_icons/aws.png"
+
 function Footer() {
     return (
         <div>
@@ -10,13 +14,27 @@ function Footer() {
                     <h3>Social media</h3>
                     <div className="icon-container">
                         <div className="icon">
-                            <img src={Linkedin} alt="linkedin" width={"100px"} height={"100px"}/>
+                            <img src={Linkedin} alt="linkedin" width={"70px"} height={"70px"}/>
                         </div>
                         <div className="icon">
-                            <img src={Medium} alt="medium" width={"100px"} height={"100px"}/>
+                            <img src={Medium} alt="medium" width={"70px"} height={"70px"}/>
                         </div>
                         <div className="icon">
-                            <img src={Github} alt="github" width={"100px"} height={"100px"}/>
+                            <img src={Github} alt="github" width={"70px"} height={"70px"}/>
+                        </div>
+                    </div>
+                </div>
+                <div className="tecnology-stacks">
+                    <h3>Tecnology Stacks</h3>
+                    <div className="icon-container">
+                        <div className="icon">
+                            <img src={Python} alt="python" width={"70px"} height={"70px"}/>
+                        </div>
+                        <div className="icon">
+                            <img src={Django} alt="Django" width={"70px"} height={"70px"}/>
+                        </div>
+                        <div className="icon">
+                            <img src={Aws} alt="aws" width={"70px"} height={"70px"}/>
                         </div>
                     </div>
                 </div>
