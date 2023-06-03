0a8ecffdbb81884c8409ed2d77fc5c2a184db469
Kassio Wallace <kassiowl34@gmail.com>
2023-06-03 20:04:18 -0300

    about me add

--- src/Personal_Info/about_me.tsx
+++ src/Personal_Info/about_me.tsx
@@ -1,7 +1,9 @@
 import "./css/about-me.css"
+import Picture from "./picture/person.png"
 function AboutMe() {
   return(
       <div className="about-me">
+        <div className="text">
         <h2>About me</h2>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
           Impedit esse soluta similique cumque quo nobis dolores labore voluptate nesciunt aliquam obcaecati repudiandae, 
@@ -15,6 +17,10 @@ function AboutMe() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
           Quibusdam, aut quas voluptatibus ipsum at rem dolorum! Distinctio error maxime officiis, vero reiciendis, 
           quis nemo neque aperiam eius perspiciatis accusantium veniam.</p>
+        </div>
+        <div className="photo">
+          <img src={Picture} alt="person" />
+        </div>
       </div>
     )
   }
