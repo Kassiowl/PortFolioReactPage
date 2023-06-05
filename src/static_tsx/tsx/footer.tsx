2dbd3a8248374179547fee28fa286925ea351732
Kassio Wallace <kassiowl34@gmail.com>
2023-06-05 14:15:52 -0300

    footer modd

--- src/static_tsx/tsx/footer.tsx
+++ src/static_tsx/tsx/footer.tsx
@@ -9,22 +9,22 @@ import { Link, BrowserRouter } from 'react-router-dom';
 
 function Footer() {
     return (
-            <footer className="footer">
-                <div className="social-media">
+            <footer classNameName="footer">
+                <div classNameName="social-media">
                     <h3>Social media</h3>
-                    <div className="icon-container">
+                    <div classNameName="icon-container">
                         <BrowserRouter>
-                        <div className="icon">
+                        <div classNameName="icon">
                             <Link to={"https://www.linkedin.com/in/kassiowallace/"} target="_blank">
                                 <img src={Linkedin} alt="linkedin" width={"70px"} height={"70px"}/>
                             </Link>
                         </div>
-                        <div className="icon">
+                        <div classNameName="icon">
                             <Link to={"https://medium.com/@kassiowl34"} target="_blank"> 
                                 <img src={Medium} alt="medium" width={"70px"} height={"70px"}/>
                             </Link>
                         </div>
-                        <div className="icon">
+                        <div classNameName="icon">
                             <Link to={"https://github.com/Kassiowl"} target="_blank">
                                 <img src={Github} alt="github" width={"70px"} height={"70px"}/>
                             </Link>
@@ -32,21 +32,21 @@ function Footer() {
                         </BrowserRouter>
                     </div>
                 </div>
-                <div className="tecnology-stacks">
+                <div classNameName="tecnology-stacks">
                     <h3>Tecnology Stacks</h3>
-                    <div className="icon-container">
+                    <div classNameName="icon-container">
                         <BrowserRouter>
-                            <div className="icon">
+                            <div classNameName="icon">
                                 <Link to={"https://www.python.org/"} target="_blank" >
                                     <img src={Python} alt="python" width={"70px"} height={"70px"}/>
                                 </Link>
                             </div>
-                            <div className="icon">
+                            <div classNameName="icon">
                                 <Link to={"https://www.djangoproject.com/"} target="_blank" >
                                     <img src={Django} alt="Django" width={"70px"} height={"70px"}/>
                                 </Link>
                             </div>
-                            <div className="icon">
+                            <div classNameName="icon">
                                 <Link to={"https://aws.amazon.com/pt/"} target="_blank" >
                                     <img src={Aws} alt="aws" width={"70px"} height={"70px"}/>
                                 </Link>
