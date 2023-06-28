f1044ead382ae244b1b6d2c9119619c48bdae429
Kassio Wallace <kassiowl34@gmail.com>
2023-06-28 18:24:41 -0300

    portfolio page add

--- src/Personal_Info/portfolio.tsx
+++ src/Personal_Info/portfolio.tsx
@@ -1,8 +1,40 @@
-
+import { Col, Container, Row } from "react-bootstrap";
+import "./css/portfolio.css"
 function Portfolio() {
   return(
     <>
-      <h2>Portfolio</h2>
+      <Container fluid="xs">
+        <Row>
+        <Col sm={12} md={8} lg={6} xxl={5}>
+          <div className="mb-4 project">
+            <h5>Project 1</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+              <a href="/">
+                <img src="https://projectideasincse.files.wordpress.com/2017/07/web-localstn.jpg" width={500} height={500}></img>
+              </a>
+          </div>
+          </Col>
+        <Col sm={12} md={8} lg={6} xxl={5}>
+          <div className="mb-4 project">
+            <h5>Project 1</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+              <a href="/">
+                <img src="https://projectideasincse.files.wordpress.com/2017/07/web-localstn.jpg" width={500} height={500}></img>
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   
   )
