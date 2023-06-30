dfc06940b6d8fcfd47053d6cb13abe293c8cf044
Kassio Wallace <kassiowl34@gmail.com>
2023-06-30 15:49:07 -0300

    testimonials add

--- src/Personal_Info/testimonials.tsx
+++ src/Personal_Info/testimonials.tsx
@@ -1,10 +1,84 @@
+import { Col, Container, Row } from "react-bootstrap";
 
 function Testimonials() {
   return(
     <>
-    <h2>Testimonials</h2>
+       <Container fluid="xs">
+        <Row>
+        <Col sm={12} md={8} lg={6} xxl={3}>
+          <div className="mb-4 mt-4 project">
+            <h5>Persons'name</h5>
+              <a href="/">
+                <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" width={50} height={50}></img>
+              </a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+          </div>
+          </Col>
+        <Col sm={12} md={8} lg={6} xxl={3}>
+          <div className="mb-4 mt-4 project">
+            <h5>Persons name</h5>
+            <a href="/">
+                <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" width={50} height={50}></img>
+              </a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+            </div>
+          </Col>
+          <Col sm={12} md={8} lg={6} xxl={3}>
+          <div className="mb-4 mt-4 project">
+            <h5>Persons name</h5>
+            <a href="/">
+                <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" width={50} height={50}></img>
+              </a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+            </div>
+          </Col>
+          <Col sm={12} md={8} lg={6} xxl={3}>
+          <div className="mb-4 mt-4 project">
+            <h5>Persons name</h5>
+            <a href="/">
+                <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" width={50} height={50}></img>
+              </a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+            </div>
+          </Col>
+          <Col sm={12} md={8} lg={6} xxl={3}>
+          <div className="mb-4 mt-4 project">
+            <h5>Persons name</h5>
+            <a href="/">
+                <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" width={50} height={50}></img>
+              </a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Omnis fuga ipsa dolores non perspiciatis deserunt dicta consequatur amet harum quidem dolor aliquid in molestias, 
+              nesciunt ab expedita quibusdam, laborum recusandae!</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
+)
   }
   
   export default Testimonials;
