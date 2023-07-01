c629a612a1eb26c6622d82fd6de34b59057e58ed
Kassio Wallace <kassiowl34@gmail.com>
2023-07-01 16:49:56 -0300

    experiences add

--- src/Personal_Info/experiences.tsx
+++ src/Personal_Info/experiences.tsx
@@ -1,8 +1,167 @@
+import { Col, Container, Row } from "react-bootstrap";
 
 function Experiences() {
   return(
     <>
-      <h2>Experiences</h2>
+      <Container>
+        <Row>
+          <Col sm={12} md={8} lg={6} xxl={4} className="mb-4 mt-4">
+            <h2>Intern Developer - Sedes</h2>
+            <h3>Activities</h3>
+            <p> - A form based software with complex business rules, validation
+              and huge feedback capabilities to the users. The form validates a
+              series of rules where the values allowed on the fields depend on
+              each other. The form supports bidirectional feedback meaning
+              that any field can invalidate the other, no matter the order
+              they've been filled.
+            </p>
+            <p>
+              - Agile development and version control using gitlab
+            </p>
+            <p> 
+              - Leadership in a small and beginner-level development team,
+              meeting with stakeholders, translating software requirements
+              and ticket creation with task delegation
+            </p>
+            <Container>
+              <Row>
+              <h4 className="text-center">Tecnologies Used</h4>
+              <Col sm={12} md={8} lg={6} xxl={5}>
+                <h5>Front-End</h5>
+              </Col>
+              <Col sm={12} md={8} lg={6} xxl={5}>
+                <h5 className="text-end">Back-End</h5>
+                <p></p>
+              </Col>
+              </Row>
+            </Container>
+          </Col>
+          <Col sm={12} md={8} lg={6} xxl={4} className="mb-4 mt-4">
+            <h2>Intern Developer - Sedes</h2>
+            <h3>Activities</h3>
+            <p> - A form based software with complex business rules, validation
+              and huge feedback capabilities to the users. The form validates a
+              series of rules where the values allowed on the fields depend on
+              each other. The form supports bidirectional feedback meaning
+              that any field can invalidate the other, no matter the order
+              they've been filled.
+            </p>
+            <p>
+              - Agile development and version control using gitlab
+            </p>
+            <p> 
+              - Leadership in a small and beginner-level development team,
+              meeting with stakeholders, translating software requirements
+              and ticket creation with task delegation
+            </p>
+            <Container>
+              <Row>
+              <h4 className="text-center">Tecnologies Used</h4>
+              <Col sm={12} md={8} lg={6} xxl={5}>
+                <h5>Front-End</h5>
+              </Col>
+              <Col sm={12} md={8} lg={6} xxl={5}>
+                <h5 className="text-end">Back-End</h5>
+                <p></p>
+              </Col>
+              </Row>
+            </Container>
+          </Col>
+          <Col sm={12} md={8} lg={6} xxl={4} className="mb-4 mt-4">
+            <h2>Intern Developer - Sedes</h2>
+            <h3>Activities</h3>
+            <p> - A form based software with complex business rules, validation
+              and huge feedback capabilities to the users. The form validates a
+              series of rules where the values allowed on the fields depend on
+              each other. The form supports bidirectional feedback meaning
+              that any field can invalidate the other, no matter the order
+              they've been filled.
+            </p>
+            <p>
+              - Agile development and version control using gitlab
+            </p>
+            <p> 
+              - Leadership in a small and beginner-level development team,
+              meeting with stakeholders, translating software requirements
+              and ticket creation with task delegation
+            </p>
+            <Container>
+              <Row>
+              <h4 className="text-center">Tecnologies Used</h4>
+              <Col sm={12} md={8} lg={6} xxl={5}>
+                <h5>Front-End</h5>
+              </Col>
+              <Col sm={12} md={8} lg={6} xxl={5}>
+                <h5 className="text-end">Back-End</h5>
+                <p></p>
+              </Col>
+              </Row>
+            </Container>
+          </Col>
+          <Col sm={12} md={8} lg={6} xxl={4} className="mb-4 mt-4">
+            <h2>Intern Developer - Sedes</h2>
+            <h3>Activities</h3>
+            <p> - A form based software with complex business rules, validation
+              and huge feedback capabilities to the users. The form validates a
+              series of rules where the values allowed on the fields depend on
+              each other. The form supports bidirectional feedback meaning
+              that any field can invalidate the other, no matter the order
+              they've been filled.
+            </p>
+            <p>
+              - Agile development and version control using gitlab
+            </p>
+            <p> 
+              - Leadership in a small and beginner-level development team,
+              meeting with stakeholders, translating software requirements
+              and ticket creation with task delegation
+            </p>
+            <Container>
+              <Row>
+              <h4 className="text-center">Tecnologies Used</h4>
+              <Col sm={12} md={8} lg={6} xxl={5}>
+                <h5>Front-End</h5>
+              </Col>
+              <Col sm={12} md={8} lg={6} xxl={5}>
+                <h5 className="text-end">Back-End</h5>
+                <p></p>
+              </Col>
+              </Row>
+            </Container>
+          </Col>
+          <Col sm={12} md={8} lg={6} xxl={4} className="mb-4 mt-4">
+            <h2>Intern Developer - Sedes</h2>
+            <h3>Activities</h3>
+            <p> - A form based software with complex business rules, validation
+              and huge feedback capabilities to the users. The form validates a
+              series of rules where the values allowed on the fields depend on
+              each other. The form supports bidirectional feedback meaning
+              that any field can invalidate the other, no matter the order
+              they've been filled.
+            </p>
+            <p>
+              - Agile development and version control using gitlab
+            </p>
+            <p> 
+              - Leadership in a small and beginner-level development team,
+              meeting with stakeholders, translating software requirements
+              and ticket creation with task delegation
+            </p>
+            <Container>
+              <Row>
+              <h4 className="text-center">Tecnologies Used</h4>
+              <Col sm={12} md={8} lg={6} xxl={5}>
+                <h5>Front-End</h5>
+              </Col>
+              <Col sm={12} md={8} lg={6} xxl={5}>
+                <h5 className="text-end">Back-End</h5>
+                <p></p>
+              </Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
