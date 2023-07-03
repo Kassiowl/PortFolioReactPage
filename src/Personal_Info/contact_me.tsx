e3b47346653562c61554f6aa489f2e0e5d2e4d38
Kassio Wallace <kassiowl34@gmail.com>
2023-07-03 15:56:03 -0300

    contact me add

--- src/Personal_Info/contact_me.tsx
+++ src/Personal_Info/contact_me.tsx
@@ -1,11 +1,25 @@
-
+import { Container, Form } from "react-bootstrap";
+import "./css/contact_me.css"
 function ContactMe() {
     return(
-      <>
-        <h2>Contact Me</h2>
-      </>
-    
-    )
+      <Container className="">
+        <Form>
+          <Form.Group className="m-4" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Form.Group className="m-4" controlId="subject">
+            <Form.Label>subject</Form.Label>
+            <Form.Control type="text"/>
+          </Form.Group>
+          <Form.Group className="m-4" controlId="message">
+            <Form.Label>message</Form.Label>
+            <Form.Control as="textarea" className="text-area-custom-css" rows={10} />
+          </Form.Group>
+          <button className="btn btn-lg btn-info ms-4">Send</button>
+        </Form>
+      </Container>
+      );
 }
 
 
