00ad4fecc697240500d5ab9754c3503927a45cc9
Kassio Wallace <kassiowl34@gmail.com>
2023-07-12 15:28:16 -0300

    cache implemented

--- api/main.py
+++ api/main.py
@@ -8,6 +8,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+
+from redis import asyncio as aioredis
+
+
 origins = [
     "*"
 ]
@@ -20,12 +28,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+@cache()
+async def get_cache():
+    kassio = Kassio()
+    return {
+                "about_me": kassio.about_me,
+                "experiences": kassio.experiences,
+                "projects": kassio.projects,
+                "testimonials": kassio.testimonials
+           }
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/kassio")
+@cache(expire=3600)
 def read_root():
     kassio = Kassio()
     return {
@@ -37,3 +58,7 @@ def read_root():
 
 
 
+@app.on_event("startup")
+async def startup():
+    redis = aioredis.from_url("redis://localhost")
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
